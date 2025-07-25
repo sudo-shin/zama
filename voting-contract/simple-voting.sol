852cc2ab06ffabc218b045a47c1000ddfafd5914
shin <jritoshinkmt@gmail.com>
2025-07-25 19:58:37 +0700

    Update simple-voting.sol

--- voting-contract/simple-voting.sol
+++ voting-contract/simple-voting.sol
@@ -1,1 +1,25 @@
+// Standard Solidity voting contract example
+pragma solidity ^0.8.0;
 
+contract SimpleVoting {
+    mapping(address => bool) public hasVoted;
+    uint64 public yesVotes;
+    uint64 public noVotes;
+    uint256 public voteDeadline;
+
+    function vote(bool support) public {
+        require(block.timestamp <= voteDeadline, "Too late to vote");
+        require(!hasVoted[msg.sender], "Already voted");
+        hasVoted[msg.sender] = true;
+
+        if (support) {
+            yesVotes += 1;
+        } else {
+            noVotes += 1;
+        }
+    }
+
+    function getResults() public view returns (uint64, uint64) {
+        return (yesVotes, noVotes);
+    }
+}
